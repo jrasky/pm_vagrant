e54770b6b127ce75160dbfeaaa9bca06c51b7fc4
Jerome Rasky <jerome@rasky.co>
2013-11-29 15:14:07 -0800

    Remove python-dev from pm_setup.sh because it's not used

--- pm_setup.sh
+++ pm_setup.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get dist-upgrade -y
-apt-get install -y python-pip python-dev
+apt-get install -y python-pip
 pip install --upgrade "django<1.5"
 pip install --upgrade "django-pipeline"
 pip install --upgrade "distribute"

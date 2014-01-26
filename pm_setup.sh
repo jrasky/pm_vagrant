65dd2ede1c5b3e75b77cbde53995ecd81432c261
Jerome Rasky <jyrome.112@gmail.com>
2014-01-25 16:15:23 -0800

    Simplified pm_setup.py

--- pm_setup.sh
+++ pm_setup.sh
@@ -3,16 +3,7 @@
 apt-get update
 apt-get dist-upgrade -y
 apt-get install -y python-pip
-pip install --upgrade "django"
-pip install --upgrade "django-pipeline"
-pip install --upgrade "distribute"
-pip install --upgrade "django-grappelli"
-pip install --upgrade "django-html5"
-pip install --upgrade "django-haystack"
-pip install --upgrade "whoosh"
-pip install --upgrade "django-widget-tweaks"
-pip install --upgrade "django_select2"
-pip install --upgrade "python-dateutil"
+pip install --upgrade "django" "django-pipeline" "distribute" "django-grappelli" "django-html5" "django-haystack" "whoosh" "django-widget-tweaks" "django_select2" "python-dateutil"
 python /vagrant/project_management/manage.py syncdb --noinput
 python /vagrant/project_management/manage.py loaddata /vagrant/project_management/initial_data.json
 python /vagrant/project_management/manage.py rebuild_index --noinput
